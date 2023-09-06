b3708123359adff2c41fc10a6234f30d2fe68a44
mantaslv <mantasvolkas@gmail.com>
2023-09-06 17:16:48 +0100

    Test for deploy

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,24 @@
-import { Box, Typography } from '@mui/material';
+import { Button, Grid, TextField, Typography } from '@mui/material';
 
 const Home = () => {
     return (
-        <Box>
-            <Typography variant='h2' component='h1'>Apply-Ally</Typography>
-        </Box>
+        <Grid 
+            container 
+            direction="column"
+            alignItems="center"
+            spacing={2}
+        >
+            <Grid item><Typography variant="h2" component="h1">Test</Typography></Grid>
+            <Grid item><Typography variant="h2" component="h1">Apply-Ally</Typography></Grid>
+            <Grid item><Typography variant="h5">Add old cv</Typography></Grid>
+            <Grid item><TextField label="Title" sx={{ width: '500px' }}/></Grid>
+            <Grid item><TextField label="Profile" multiline sx={{ width: '700px' }}/></Grid>
+            <Grid item><TextField label="Education" multiline sx={{ width: '700px' }}/></Grid>
+            <Grid item><TextField label="Projects" multiline sx={{ width: '700px' }}/></Grid>
+            <Grid item><TextField label="Professional Experience" multiline sx={{ width: '700px' }}/></Grid>
+            <Grid item><TextField label="Interests" multiline sx={{ width: '700px' }}/></Grid>
+            <Grid item><Button variant="contained">Submit</Button></Grid>
+        </Grid>
     );
 }
 
